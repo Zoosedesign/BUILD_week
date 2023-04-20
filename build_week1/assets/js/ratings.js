2fdc5b4a4d568f4f817a8aac30b64275d13fbe25
Zoosedesign <andre.zucchetti@me.com>
2023-04-20 16:56:15 +0200

    def

--- build_week1/assets/js/ratings.js
+++ build_week1/assets/js/ratings.js
@@ -1,24 +1,24 @@
-var stars2 = document.querySelectorAll(".star img");
+var stars2 = document.querySelectorAll('.star img');
 var BtnFinale = document.getElementById('btnFinale');
 var vuoto = document.getElementById('testo'); 
 
-
 window.addEventListener('load', function () {
-    vuoto.value = "";
-    stars2.value = "";
+    vuoto.value = '';
+    stars2.value = '';
 });
 
 stars2.forEach((item, index1) => {
-    item.addEventListener('click', () => {  //
+    item.addEventListener('click', () => { 
       stars2.forEach((star2, index2) => {
         index1 <= index2 ? star2.classList.add('active') : star2.classList.remove('active');
-        let value = parseInt(item.getAttribute("value"));
+        let value = parseInt(item.getAttribute('value'));
             console.log(value);
       })
     } )
   })
+
 BtnFinale.addEventListener('click', function () {
-  vuoto.value = "";
+  vuoto.value = '';
   stars2.forEach((star2) => {
     star2.classList.remove('active');
   });
---
+++ build_week1/assets/js/result.js
@@ -0,0 +1,55 @@
+// VARIABILI GLOBALI RISULTATI
+// risposte corrette
+let scoreRight = localStorage.getItem('score');
+// numero risposte
+let quizLength = localStorage.getItem('totQst')
+// risposte sbagliate 
+let scoreWrong = (quizLength - scoreRight);
+// percentuale risposte giuste
+let percentRight = ((scoreRight / quizLength) * 100.).toFixed(1);
+// percentuale risposte sbagliate
+let percentWrong = (100 - percentRight).toFixed(1);
+// esito del test
+function stato() {
+    if (percentRight >= 60) {
+        return '<h5>Congratulations!<br><strong class="light-blue">You passed the exam.</strong></h5><p>We\'ll send you the certificate in few minutes.Check your email (including promotions / spam folder<p>';
+    } else {
+        return '<h5>Oh sorry!<br><strong class="light-blue">You failed the exam.</strong></h5><p>The teacher will get in touch with you to understand your mistakes, you will definitely improve in the future</p>';
+    }
+}
+
+let status = stato();
+
+// POPOLIAMO IL DIV RISPOSTE CORRETTE
+const correct = document.getElementsByClassName('correct')[0];
+const wrong = document.getElementsByClassName('wrong')[0];
+const correctPercentage = document.createElement('p');
+correctPercentage.innerHTML = `${percentRight}%`;
+correct.appendChild(correctPercentage);
+const correctAnswers = document.createElement('p');
+correctAnswers.innerHTML = `<span id="contatore">${scoreRight}</span>/${quizLength}questions`;
+correct.appendChild(correctAnswers);
+
+// POPOLIAMO IL DIV RISPOSTE SBAGLIATE 
+const wrongPercentage = document.createElement('p');
+wrongPercentage.innerHTML = `${percentWrong}%`;
+wrong.appendChild(wrongPercentage);
+const wrongAnswers = document.createElement('p');
+wrongAnswers.innerHTML = `<span id="contatore">${scoreWrong}</span>/${quizLength}questions`;
+wrong.appendChild(wrongAnswers);
+
+// POPOLIAMO IL DIV ESITO DEL TEST
+const round = document.getElementsByClassName('round')[0];
+round.innerHTML = status;
+
+// DIAMO LA PERCENTUALE ALLA TORTA
+function Color() {
+    let strokeColor = document.getElementById('circle4').setAttribute('stroke-dasharray', `${percentRight / 100}, 100`);
+    return strokeColor;
+}
+Color()
+
+let btnResults = document.getElementById('btnResults');
+btnResults.addEventListener('click', function () {
+    location.href = 'rating.html';
+})
