2c579e94da06568375ff56af6daa87ca9c0e244d
Zoosedesign <andre.zucchetti@me.com>
2023-04-21 09:57:53 +0200

    def

--- build_week1/assets/js/ratings.js
+++ build_week1/assets/js/ratings.js
@@ -1,23 +1,35 @@
 var stars2 = document.querySelectorAll('.star img');
-var BtnFinale = document.getElementById('btnFinale');
+var btnFinale = document.getElementById('btnFinale');
 var vuoto = document.getElementById('testo'); 
+const app = document.getElementsByClassName('center')[1];
 
 window.addEventListener('load', function () {
     vuoto.value = '';
-    stars2.value = '';
 });
 
 stars2.forEach((item, index1) => {
     item.addEventListener('click', () => { 
       stars2.forEach((star2, index2) => {
         index1 <= index2 ? star2.classList.add('active') : star2.classList.remove('active');
-        let value = parseInt(item.getAttribute('value'));
+        let value = parseInt(item.getAttribute('value-star'));
+        sessionStorage.setItem('value-star', value)
             console.log(value);
       })
     } )
   })
 
-BtnFinale.addEventListener('click', function () {
+btnFinale.addEventListener('click', function () {
+  if (vuoto.value == "" || !sessionStorage.getItem('value-star')){
+    alert('Write a comment and rate us!')
+  } else {
+    sessionStorage.setItem('testo', vuoto);
+    btnFinale.setAttribute('disabled', 'true');
+    vuoto.classList.add('off');
+    const grazie = document.createElement('p');
+    vuoto.appendChild(grazie);
+    app.innerHTML = `<h4 class="pink">Thank you, your reply has been recorded!</h4>`;
+  }
+
   vuoto.value = '';
   stars2.forEach((star2) => {
     star2.classList.remove('active');
